7cff53cf9f2673a995f1bdebf5d61261996f48ca
blackmama12 <arunkumar773681@gmail.com>
2026-01-21 16:13:23 +0530

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -82,7 +82,7 @@ export default async (sock, msg, args) => {
 ┃ ⊙🖼️ .Sticker–Turn image/video/gif into a sticker.
 ┃ ⊙🎲 .Game–Play built-in games.
 ┃ ⊙🎭 .Fun–Jokes and fun activities.
-┃ ⊙🤖 .Ai <text>–Chat with the AI.
+┃ ⊙🔍 .Search–Explore any Information
 ┃ ⊙🆎️ .Font <text>–Change text to stylish fonts.
 ┃ ⊙👤 .Owner–Get creator's info.
 ┃ ⊙🤯 .Play–built-in games.
